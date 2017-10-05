8cb04eda849c9c4718b55f3bc5ae4f0dad966a51
odan <odan@users.noreply.github.com>
2017-10-05 11:10:58 +0200

    Updated docs

--- tests/performance.php
+++ tests/performance.php
@@ -11,57 +11,6 @@ $arr['email'] = "mail@example.com";
 $arr['phone'] = "123456";
 $arr['not existing item'] = "test";
 
-// User entity
-class User
-{
-    public $id;
-    public $username;
-    public $firstName;
-    public $email;
-}
-
-// User row (from database)
-$userRow = [
-    'id' => 1,
-    'username' => 'admin',
-    'first_name' => 'John Doe',
-    'email' => 'john@exmaple.com'
-];
-
-// Create the hydrator
-$hydrator = new \Odan\Hydrator\ObjectProperty();
-
-// Convert array to a new User object (with lower camel case properties)
-$user = $hydrator->hydrate($userRow, new User());
-print_r($user);
-
-/*
-User Object
-(
-    [id] => 1
-    [username] => admin
-    [firstName] => John Doe
-    [email] => john@exmaple.com
-)
-*/
-
-// Convert User object to an array with lower camel case keys
-$array = $hydrator->extract($user);
-
-print_r($array);
-
-/*
-Array
-(
-    [id] => 1
-    [username] => admin
-    [firstName] => John Doe
-    [email] => john@exmaple.com
-)
-*/
-
-exit;
-
 $start = microtime(true);
 
 for ($i = 0; $i < 10000; $i++) {
